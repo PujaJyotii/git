e67d92730616aaa1c3e17089815d6073f7c2e66d
PujaJyotii <pujajyoti111>
2023-07-03 13:54:35 +0530

    question Five

--- dom.js
+++ dom.js
@@ -8,12 +8,27 @@ var title = document.querySelector('.title');
 title.style.color = 'green'; 
 title.textContent = 'ADD ITEMS';*/
 
-var item=document.getElementsByClassName('list-group-item')
+/*var item=document.getElementsByClassName('list-group-item')
 item[2].style.backgroundColor='green'
 for(var i=0;i<item.length;i++)
 {
     item[i].style.fontWeight='bold';
     item[i].style.color='purple'
-}
+}*/
+
+
+//GETELEMENTBYCLASS
+ var li=document.getElementsByTagName('li')
+ console.log(li)
+ 
+ console.log(li[1])
+ li[1].textContent='Hello 2'
+ li[1].style.fontWeight='bold'
+ li[1].style.backgroundColor='green'
+ 
+ for(var i=0;i<li.length;i++)
+ {
+    li[i].style.backgroundColor='#f4f4f4'
+ }
 
 
